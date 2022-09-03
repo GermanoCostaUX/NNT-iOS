27e122292f95d95ce2c8cb8b8a5ae1405b2aad2f
GermanoCostaUX <cgermanofcosta.nnt@gmail.com>
2022-09-03 19:04:12 -0400

    first commit

--- testApp/ViewController.swift
+++ testApp/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func inputEmail(_ sender: Any) {
+        print("Email Inserted")
+    }
+    
+    @IBAction func inputPassword(_ sender: Any) {
+        print("Password Inserted")
+    }
+    
+    
+    @IBAction func confirmButton(_ sender: Any) {
+        print("Confirm Button Pressed")
+    }
+    
 }
 
